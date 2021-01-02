785dd0fa6abcc1bfd30463d4d035997a3718c00d
Jubery <76787670+jubery-jahan@users.noreply.github.com>
2021-01-02 19:35:47 +0600

    Add files via upload

---
+++ uri2.java
@@ -0,0 +1,19 @@
+
+package uri;
+
+import java.util.Scanner;
+public class uri2 {
+    public static void main(String[] args) {
+        int A,B,x;
+        Scanner s=new Scanner(System.in);
+	System.out.println("Enter any two numbers :");
+	A=s.nextInt();
+	B=s.nextInt();
+	x=A+B;
+	System.out.println("Sum: "+x);
+	}
+}
+        
+    
+    
+
