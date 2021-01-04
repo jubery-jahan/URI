02e95f8c93603aed8777ead1c8ad023e56ba51f3
Jubery <76787670+jubery-jahan@users.noreply.github.com>
2021-01-04 21:32:32 +0600

    Add files via upload

---
+++ simple_sum.java
@@ -0,0 +1,16 @@
+
+package uri;
+
+import java.util.Scanner;
+public class simple_sum {
+     public static void main(String[] args) {
+        int A,B,SOMA;
+        Scanner s=new Scanner(System.in);
+	System.out.println("Enter any two numbers :");
+	A=s.nextInt();
+	B=s.nextInt();
+	SOMA=A+B;
+	System.out.println("Sum: "+SOMA);
+	}
+}
+
