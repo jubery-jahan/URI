1d290be0dbfbbde3f8360bd5b1f43220e5004dc8
Jubery <76787670+jubery-jahan@users.noreply.github.com>
2021-01-05 19:38:05 +0600

    Add files via upload

---
+++ simple_product1004.java
@@ -0,0 +1,18 @@
+
+package uri;
+
+import java.util.Scanner;
+
+
+public class simple_product1004 {
+   
+     public static void main(String[] args) {
+        int A,B,PROD;
+        Scanner s=new Scanner(System.in);
+	System.out.println("Enter any two numbers :");
+	A=s.nextInt();
+	B=s.nextInt();
+	PROD=A*B;
+	System.out.println("PROD = "+PROD);
+	}
+}
