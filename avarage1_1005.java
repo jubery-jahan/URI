11de42e6c1a6fd1fbacc043770e56f6e4a017928
Jubery <76787670+jubery-jahan@users.noreply.github.com>
2021-01-05 19:47:16 +0600

    Add files via upload

---
+++ avarage1_1005.java
@@ -0,0 +1,19 @@
+
+package uri;
+
+import java.util.Scanner;
+
+public class avarage1_1005 {
+     public static void main(String[] args){
+
+     float A, B, med;
+            Scanner sc = new Scanner(System.in);
+            A = sc.nextFloat();
+            B = sc.nextFloat();
+            med = (float) (((A * 3.5) + (B * 7.5)) / (3.5 + 7.5));
+            String media = String.format("MEDIA = %,.5f", med);
+            System.out.print(media +"\n");
+
+    
+     }
+}
