333d67763035c7ff56e04edc5c89172c5daf123d
Jubery <76787670+jubery-jahan@users.noreply.github.com>
2021-01-06 20:03:09 +0600

    Add files via upload

---
+++ Diffrence_1007.java
@@ -0,0 +1,15 @@
+
+package uri;
+import java.util.Scanner;
+public class Diffrence_1007 {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        int a,b,c,d,DIFFERENCE ;
+        a=sc.nextInt();
+        b=sc.nextInt();
+        c=sc.nextInt();
+        d=sc.nextInt();
+        DIFFERENCE=a*c-b*d;
+        System.out.println("DIFFERENCE="+DIFFERENCE);
+    }
+}
