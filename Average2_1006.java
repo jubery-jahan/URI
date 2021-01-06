02c6d085c7422c8d145a34c7a6b6f327fc9630ad
Jubery <76787670+jubery-jahan@users.noreply.github.com>
2021-01-06 19:47:36 +0600

    Add files via upload

---
+++ Average2_1006.java
@@ -0,0 +1,17 @@
+
+package uri;
+
+import java.util.Scanner;
+
+public class Average2_1006 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double a, b, c, MEDIA;
+        a=sc.nextDouble();
+         b=sc.nextDouble();
+          c=sc.nextDouble();
+          MEDIA= (a*2+b*3+c*5)/10;
+          System.out.println("\"MEDIA = %,.1f\", MEDIA");
+        
+    }
+}
